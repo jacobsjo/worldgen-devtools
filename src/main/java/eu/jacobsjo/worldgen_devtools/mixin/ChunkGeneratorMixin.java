a94dadb31b8933395a2930cf2baf00f51587e17c
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-01-05 19:25:33 +0100

    fix crash when reloading during worldgen

---
+++ src/main/java/eu/jacobsjo/worldgen_devtools/mixin/ChunkGeneratorMixin.java
@@ -0,0 +1,27 @@
+package eu.jacobsjo.worldgen_devtools.mixin;
+
+import it.unimi.dsi.fastutil.ints.IntSet;
+import net.minecraft.world.level.biome.FeatureSorter;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ChunkGenerator.class)
+public class ChunkGeneratorMixin {
+
+    /**
+     * @author jacobsjo
+     * @reason overwriting simple lambda
+     */
+    @Overwrite
+    private static void method_39788(IntSet intSet, FeatureSorter.StepFeatureData stepFeatureData, PlacedFeature placedFeature){
+        int id = stepFeatureData.indexMapping().applyAsInt((PlacedFeature)placedFeature);
+        /*
+         if id is -1 then the placedFeature wasn't found in the indexMapping. This happens when biomeDecoration happends
+         during reload but the featureSorter hasn't rerun yet. This can cause some chunks with missinge features.
+         */
+        if (id >= 0)
+            intSet.add(id);
+    }
+}
