579883b5aac58b9690b8ac416d4cef2e3787bbd1
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-01-10 01:43:20 +0100

    added worldgen settings gamerules

---
+++ src/main/java/eu/jacobsjo/worldgen_devtools/worldgen_settings/WorldgenSettingsInit.java
@@ -0,0 +1,24 @@
+package eu.jacobsjo.worldgen_devtools.worldgen_settings;
+
+import eu.jacobsjo.worldgen_devtools.worldgen_settings.api.GenerationOptions;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
+import net.minecraft.world.level.GameRules;
+
+public class WorldgenSettingsInit implements ModInitializer {
+    public static GameRules.Key<EnumRule<GenerationOptions>> MAX_CHUNK_STATUS;
+    public static GameRules.Key<GameRules.BooleanValue> APPLY_PROCESSORS;
+    public static GameRules.Key<GameRules.BooleanValue> APPLY_JIGSAW_REPLACEMENT_PROCESSOR;
+    public static GameRules.Key<GameRules.BooleanValue> APPLY_GRAVITY_PROCESSOR;
+
+    @Override
+    public void onInitialize() {
+        MAX_CHUNK_STATUS = GameRuleRegistry.register("maxChunkStatus", GameRules.Category.MISC, GameRuleFactory.createEnumRule(GenerationOptions.ALL));
+        APPLY_PROCESSORS = GameRuleRegistry.register("applyProcessors", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+        APPLY_JIGSAW_REPLACEMENT_PROCESSOR = GameRuleRegistry.register("applyJigsawReplacementProcessor", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+        APPLY_GRAVITY_PROCESSOR = GameRuleRegistry.register("applyGravityProcessor", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+    }
+
+}
