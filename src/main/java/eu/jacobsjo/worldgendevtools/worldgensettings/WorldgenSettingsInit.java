9fae9f7b4e215e11746c8ee5cfdbbbe6b8741805
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-05-03 19:02:16 +0200

    add warning when breaking or placing blocks with chunk saving disabled.

--- src/main/java/eu/jacobsjo/worldgendevtools/worldgensettings/WorldgenSettingsInit.java
+++ src/main/java/eu/jacobsjo/worldgendevtools/worldgensettings/WorldgenSettingsInit.java
@@ -2,9 +2,17 @@ package eu.jacobsjo.worldgendevtools.worldgensettings;
 
 import eu.jacobsjo.worldgendevtools.worldgensettings.api.GenerationOptions;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.gamerule.v1.rule.EnumRule;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
 
 public class WorldgenSettingsInit implements ModInitializer {
@@ -14,6 +22,8 @@ public class WorldgenSettingsInit implements ModInitializer {
     public static GameRules.Key<GameRules.BooleanValue> APPLY_GRAVITY_PROCESSOR;
     public static GameRules.Key<GameRules.BooleanValue> SAVE_CHUNKS;
 
+    public static final Component NO_SAVE_WARNING = Component.translatableWithFallback("worldgendevtools.worldgensettings.no_save_warning", "Chunk saving is disabled").withStyle(ChatFormatting.RED);
+
     @Override
     public void onInitialize() {
         MAX_CHUNK_STATUS = GameRuleRegistry.register("maxChunkStatus", GameRules.Category.MISC, GameRuleFactory.createEnumRule(GenerationOptions.ALL));
@@ -21,6 +31,25 @@ public class WorldgenSettingsInit implements ModInitializer {
         KEEP_JIGSAWS = GameRuleRegistry.register("keepJigsaws", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
         APPLY_GRAVITY_PROCESSOR = GameRuleRegistry.register("applyGravityProcessor", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
         SAVE_CHUNKS = GameRuleRegistry.register("saveChunks", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(true));
+
+        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> {
+            if (!world.getGameRules().getRule(SAVE_CHUNKS).get()){
+                showWarning(player);
+            }
+        });
+
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (!player.isSpectator() && !world.getGameRules().getRule(SAVE_CHUNKS).get()){
+                showWarning(player);
+            }
+            return InteractionResult.PASS;
+        });
+    }
+
+    private void showWarning(Player player){
+        if (player instanceof ServerPlayer serverPlayer){
+            serverPlayer.connection.send(new ClientboundSetActionBarTextPacket(NO_SAVE_WARNING));
+        }
     }
 
 }
