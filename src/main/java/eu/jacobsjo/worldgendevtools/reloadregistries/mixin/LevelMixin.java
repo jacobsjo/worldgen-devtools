96472fdc3cd8c8c5783340a8a26f6138fda03be2
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-09-11 20:35:06 +0200

    update to 24w37a

--- src/main/java/eu/jacobsjo/worldgendevtools/reloadregistries/mixin/LevelMixin.java
+++ src/main/java/eu/jacobsjo/worldgendevtools/reloadregistries/mixin/LevelMixin.java
@@ -6,7 +6,6 @@ import eu.jacobsjo.worldgendevtools.reloadregistries.impl.FrozenHolder;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.storage.WritableLevelData;
@@ -16,8 +15,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Supplier;
-
 @Mixin(Level.class)
 public class LevelMixin {
 
@@ -25,7 +22,7 @@ public class LevelMixin {
 
     // freeze dimensionType holder, so it doesn't get automatically updated, causing potential crashes when changing world height. DimensionType gets reset manually by resetter.
     @Inject(method = "<init>", at=@At("TAIL"))
-    public void freezeHolder(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, Supplier<ProfilerFiller> profiler, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates, CallbackInfo ci){
+    public void freezeHolder(WritableLevelData levelData, ResourceKey<Level> dimension, RegistryAccess registryAccess, Holder<DimensionType> dimensionTypeRegistration, boolean isClientSide, boolean isDebug, long biomeZoomSeed, int maxChainedNeighborUpdates, CallbackInfo ci){
         this.frozenDimensionTypeRegistration = new FrozenHolder<>(dimensionTypeRegistration);
     }
 
