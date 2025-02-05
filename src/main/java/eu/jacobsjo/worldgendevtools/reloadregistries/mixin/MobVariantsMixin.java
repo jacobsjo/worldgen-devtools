11fe38ec302bee14f27e74d9e002c840ff0a05ed
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-02-06 00:03:09 +0100

    update to 25w06a

--- src/main/java/eu/jacobsjo/worldgendevtools/reloadregistries/mixin/MobVariantsMixin.java
+++ src/main/java/eu/jacobsjo/worldgendevtools/reloadregistries/mixin/MobVariantsMixin.java
@@ -3,6 +3,7 @@ package eu.jacobsjo.worldgendevtools.reloadregistries.mixin;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import eu.jacobsjo.worldgendevtools.reloadregistries.api.OutdatedHolder;
 import net.minecraft.core.Holder;
+import net.minecraft.world.entity.animal.ChickenVariants;
 import net.minecraft.world.entity.animal.CowVariants;
 import net.minecraft.world.entity.animal.PigVariants;
 import net.minecraft.world.entity.animal.WolfVariants;
@@ -11,11 +12,11 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.stream.Stream;
 
-@Mixin({WolfVariants.class, PigVariants.class, CowVariants.class})
+@Mixin({WolfVariants.class, PigVariants.class, CowVariants.class, ChickenVariants.class})
 public class MobVariantsMixin {
 
     // Don't spawn outdated variant
-    @ModifyExpressionValue(require = 1, method = {"selectVariantToSpawn", "method_67139", "method_66314", "method_67354"}, remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;listElements()Ljava/util/stream/Stream;", remap = true))
+    @ModifyExpressionValue(require = 1, method = {"selectVariantToSpawn", "method_67139", "method_66314", "method_67354", "method_67527"}, remap = false, at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;listElements()Ljava/util/stream/Stream;", remap = true))
     private static Stream<Holder.Reference<?>> filterSpawnVariantHolders(Stream<Holder.Reference<?>> original){
         return original.filter(holder -> !((OutdatedHolder) holder).worldgenDevtools$isOutdated());
     }
