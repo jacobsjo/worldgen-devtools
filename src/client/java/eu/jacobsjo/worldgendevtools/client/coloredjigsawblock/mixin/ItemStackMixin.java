4e1a3c3958d9a51bb025b39c357a8d9520415456
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-03-06 03:54:32 +0100

    update to 25w10a

--- src/client/java/eu/jacobsjo/worldgendevtools/client/coloredjigsawblock/mixin/ItemStackMixin.java
+++ src/client/java/eu/jacobsjo/worldgendevtools/client/coloredjigsawblock/mixin/ItemStackMixin.java
@@ -1,17 +1,19 @@
 package eu.jacobsjo.worldgendevtools.client.coloredjigsawblock.mixin;
 
 import com.mojang.serialization.DataResult;
-import com.mojang.serialization.MapDecoder;
 import eu.jacobsjo.worldgendevtools.client.coloredjigsawblock.impl.JigsawBlockData;
-import net.minecraft.core.component.DataComponentGetter;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.component.DataComponentHolder;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -19,13 +21,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
 import java.util.function.Consumer;
 
-@Mixin(CustomData.class)
-public abstract class CustomDataMixin {
-    @Shadow @Final private CompoundTag tag;
-    @Shadow public abstract <T> DataResult<T> read(MapDecoder<T> mapDecoder);
+@Mixin(ItemStack.class)
+public abstract class ItemStackMixin implements DataComponentHolder {
+    @Shadow public abstract boolean is(Item item);
 
     @Unique
     private static final ResourceLocation EMPTY_RESOURCE_LOCATION = ResourceLocation.withDefaultNamespace("empty");
@@ -33,13 +33,12 @@ public abstract class CustomDataMixin {
     @SuppressWarnings("deprecation")
     private static final ResourceLocation JIGSAW_BLOCK_ID = BlockEntityType.JIGSAW.builtInRegistryHolder().key().location();
 
-    @Inject(method = "addToTooltip", at=@At("TAIL"))
-    public void addToTooltip(Item.TooltipContext tooltipContext, Consumer<Component> consumer, TooltipFlag tooltipFlag, DataComponentGetter dataComponentGetter, CallbackInfo ci){
-        Optional<String> id = this.tag.getString("id");
-        if (id.isEmpty()) return;
-        ResourceLocation resourceLocation = ResourceLocation.tryParse(id.get());
-        if (JIGSAW_BLOCK_ID.equals(resourceLocation)) {
-            DataResult<JigsawBlockData> data = this.read(JigsawBlockData.CODEC);
+    @Inject(method = "addDetailsToTooltip", at=@At("TAIL"))
+    public void addDetailsToTooltip(Item.TooltipContext tooltipContext, TooltipDisplay tooltipDisplay, Player player, TooltipFlag tooltipFlag, Consumer<Component> consumer, CallbackInfo ci){
+        if (this.is(Items.JIGSAW) && tooltipDisplay.shows(DataComponents.BLOCK_ENTITY_DATA)) {
+            CustomData customData = this.getOrDefault(DataComponents.BLOCK_ENTITY_DATA, CustomData.EMPTY);
+            DataResult<JigsawBlockData> data = customData.read(JigsawBlockData.CODEC);
+
             if (data.isError() || data.result().isEmpty())
                 return;
 
