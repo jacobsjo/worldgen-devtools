f2c4ec0f289b33bb6ffac2069245fb93901e264b
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-03-24 20:47:35 +0100

    use reconfigure by fabric api

--- src/main/java/eu/jacobsjo/worldgendevtools/reloadregistries/api/SwitchToConfigurationCallback.java
+++
@@ -1,8 +0,0 @@
-package eu.jacobsjo.worldgendevtools.reloadregistries.api;
-
-public interface SwitchToConfigurationCallback {
-    interface Callback {
-        void handle();
-    }
-    void worldgenDevtools$onSwitchToConfiguration(SwitchToConfigurationCallback.Callback callback);
-}
