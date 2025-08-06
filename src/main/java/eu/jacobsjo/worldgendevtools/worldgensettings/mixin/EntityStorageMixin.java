8fc49b3519dc6b3a015ac3659b566d84eaea92e2
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-08-07 01:41:59 +0200

    Don't save entities when saveChunks gamerule is false
    fixes #7

---
+++ src/main/java/eu/jacobsjo/worldgendevtools/worldgensettings/mixin/EntityStorageMixin.java
@@ -0,0 +1,28 @@
+package eu.jacobsjo.worldgendevtools.worldgensettings.mixin;
+
+import eu.jacobsjo.worldgendevtools.worldgensettings.WorldgenSettingsInit;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.chunk.storage.EntityStorage;
+import net.minecraft.world.level.entity.ChunkEntities;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityStorage.class)
+public class EntityStorageMixin {
+
+    @Shadow
+    @Final
+    private ServerLevel level;
+
+    @Inject(method="storeEntities", at=@At("HEAD"), cancellable = true)
+    private void save(ChunkEntities<Entity> entities, CallbackInfo ci){
+        if (!this.level.getGameRules().getBoolean(WorldgenSettingsInit.SAVE_CHUNKS)){
+            ci.cancel();
+        }
+    }
+}
